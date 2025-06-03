ed19f4a368a2a297f963f648cc2ce4f56e0b926b
Ayush Raj Sharma <ayushrajshrm@gmail.com>
2025-06-04 05:18:47 +0530

    youtube

--- rasila-frontend/src/pages/Portfolio.jsx
+++ rasila-frontend/src/pages/Portfolio.jsx
@@ -8,14 +8,14 @@ export default function Portfolio() {
 
       <div className="portfolio-grid">
         <iframe
-          src="https://www.youtube.com/embed/videoseries?list=UU-AgQ4Z6QDG3CrQK6U3KtnQ"
-          title="Rasila Infotainment Videos"
-          allowFullScreen
+          src="https://www.youtube.com/embed/dkx5Tod6o6k?si=gNK56no3lQyLqWlq&amp;controls=0"
+          title="Trahimam by Ayush Raj Sharma"
+          allowFullScreen autoplay
         />
         <iframe
-          src="https://www.youtube.com/embed/B2UnZNhU7kk"
+          src="https://www.youtube.com/embed/K73JaxW6rGY?si=UHJjDc-8B3-kSrRi&amp;controls=0"
           title="Sample Video"
-          allowFullScreen
+          allowFullScreen autoplay 
         />
       </div>
     </section>
